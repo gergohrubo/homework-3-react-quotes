7e5ec245528f21afe642b792de401d906b133cf2
gergohrubo <gergohrubo@gmail.com>
2019-11-22 21:01:22 +0100

    Like-based conditional styling added

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import './Quote.css'
 
-export default function Quote(props) {
-  return (
-    <div>
-      <p>{props.text}</p>
-      <p>By: {props.author}</p>
-    </div>
-  )
+class Quote extends Component {
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+  };
+  state = {
+    isLiked: 0
+  }
+  render() {
+    return (
+      <div>
+        <p style={
+          this.state.isLiked === -1 ? { color: 'red' }
+            : this.state.isLiked ? { color: 'green' }
+              : { color: 'black' }
+        }>
+          {this.props.text}
+        </p>
+        <p className="inlineDisplay" style={
+          this.state.isLiked === -1 ? { color: 'red' }
+            : this.state.isLiked ? { color: 'green' }
+              : { color: 'black' }
+        }>
+          By: {this.props.author}
+        </p>
+        <button onClick={() => this.setState({ isLiked: 1 })}>:)</button>
+        <button onClick={() => this.setState({ isLiked: -1 })}>:(</button>
+      </div>
+    );
+  }
 }
+
+export default Quote;
